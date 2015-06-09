18452a7811919074b88b44eb45357922e0ab0b4a
杨烽 <smartwalle@gmail.com>
2015-06-09 11:23:11 +0800

    update

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
+
 import (
-	"github.com/smartwalle/going/open_platform"
-//	"fmt"
-//	"github.com/smartwalle/going/http"
-	"fmt"
 )
 
 func main() {
-
-//	var wb = open_platform.Weibo{}
-//	var r, er = wb.CheckAccessToken("2.00kLXhIG9Cf3tD0015e73a690SCzU5")
-//	fmt.Println(r, er)
-
-	var fb = open_platform.Facebook{}
-	var s, err = fb.CheckAccessToken("CAAU7ErrNaQcBADckp3MKVj6iyLVAWUusD4xdLPbjJ97Aq6W8NuiEBMSXZBYfsLV8UN5jBsNXWZA2yfwfZCuuPwZBWiaNk8xlE3ZAUhUSRvZB6ZBUdt2lrvBf3P98N326c18iQXdJaZAoyAIUmBGHUXG4Fic4uElQZAorxRUBQXhFbZADnAGsZAmygtKxWiIZCfiFy3tLQAeGZC2YA3uuAmBefVMtR")
-	fmt.Println(s, err)
-
-	var a, e = fb.GetUserInfo("564882523638640", "CAAU7ErrNaQcBADckp3MKVj6iyLVAWUusD4xdLPbjJ97Aq6W8NuiEBMSXZBYfsLV8UN5jBsNXWZA2yfwfZCuuPwZBWiaNk8xlE3ZAUhUSRvZB6ZBUdt2lrvBf3P98N326c18iQXdJaZAoyAIUmBGHUXG4Fic4uElQZAorxRUBQXhFbZADnAGsZAmygtKxWiIZCfiFy3tLQAeGZC2YA3uuAmBefVMtR")
-	fmt.Println(a, e)
 }
--- open_platform/access_token.go
+++
@@ -1,7 +0,0 @@
-package open_platform
-
-
-type AccessToken struct {
-	UserId 		string
-	ExpireIn 	int64
-}
