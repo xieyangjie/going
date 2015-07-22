051bb6bb9a1fce361c545c97a152eb3f205a6e4e
杨烽 <smartwalle@gmail.com>
2015-07-22 13:06:07 +0800

    update

--- convert/convert.go
+++ convert/convert.go
@@ -2,21 +2,14 @@ package convert
 
 import (
 	"strconv"
+	"reflect"
 )
 
 func ConvertToString(value interface{}) string {
 	if v, ok := value.(string); ok {
 		return v
-	} else if v, ok := value.(uint64); ok {
-		return strconv.FormatUint(v, 10)
-	} else if v, ok := value.(int64); ok {
-		return strconv.FormatInt(v, 10)
-	} else if v, ok := value.(float64); ok {
-		return strconv.FormatFloat(v, 'f', -1, 64)
-	} else if v, ok := value.(bool); ok {
-		return strconv.FormatBool(v)
 	}
-	return ""
+	return strconv.FormatFloat(ConvertToFloat64(value), 'f', -1, 64)
 }
 
 func ConvertToBool(value interface{}) bool {
@@ -68,13 +61,8 @@ func ConvertToInt64(value interface{}) int64 {
 func ConvertToUint(value interface{}) uint64 {
 	if v, ok := value.(uint64); ok {
 		return v
-	} else if v, ok := value.(string); ok {
-		vi, err := strconv.ParseUint(v, 0, 64)
-		if err == nil {
-			return vi
-		}
 	}
-	return 0
+	return uint64(ConvertToFloat64(value))
 }
 
 func ConvertToFloat32(value interface{}) float32 {
@@ -87,10 +75,31 @@ func ConvertToFloat32(value interface{}) float32 {
 func ConvertToFloat64(value interface{}) float64 {
 	if v, ok := value.(float64); ok {
 		return v
-	} else if v, ok := value.(string); ok {
-		vf, err := strconv.ParseFloat(v, 64)
+	}
+	return floatValue(value)
+}
+
+func floatValue(value interface{}) float64 {
+	var vf = reflect.ValueOf(value)
+	var k = vf.Kind()
+
+	switch k {
+	case reflect.Bool:
+		 var v = vf.Bool()
+		if v {
+			return 1.0
+		}
+		return 0.0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return float64(vf.Uint())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return float64(vf.Int())
+	case reflect.Float32, reflect.Float64:
+		return vf.Float()
+	case reflect.String:
+		var f, err = strconv.ParseFloat(vf.String(), 64)
 		if err == nil {
-			return vf
+			return f
 		}
 	}
 	return 0.0
