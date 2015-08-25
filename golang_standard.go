92b4ad69cdf4038d605eac72e56b2563c82a78a1
Yangfeng <smartwalle@gmail.com>
2015-08-25 21:41:13 +0800

    update golang_standard.go

--- golang_standard.go
+++ golang_standard.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 )
 
-//常量统一采用大写，单词之前采用下划线分隔
+//常量统一采用大写，单词之间采用下划线分隔
 const (
 	CONST_A = "a"
 	CONST_B = "b"
@@ -19,22 +19,49 @@ var (
 	CreatedBy string = "SmartWalle"
 )
 
+////////////////////////////////////////////////////////////////////////////////
+//init 函数
+func init() {
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//接口以大写单词“I”开头
 type IUser interface {
 }
 
+////////////////////////////////////////////////////////////////////////////////
 type TestUser struct {
 	Username string
 	Password string
 }
 
+//创建实例的函数
 func NewTestUser() *TestUser {
 	var user = &TestUser{}
 	return user
 }
 
-func (this *TestUser)Method1() {
+//类方法
+func (this *TestUser) Method1() {
 	fmt.Println("test")
 }
 
+////////////////////////////////////////////////////////////////////////////////
+type TestStruct struct {
+	Property1 string
+	Property2 string
+}
+
+func NewTestStruct() *TestStruct {
+	var st = &TestStruct{}
+	return st
+}
+
+func (this *TestStruct) Method1() {
+	fmt.Println("test")
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//其它函数
 func Func() {
 }
