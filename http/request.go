2d3bac4a4caecb8c5538c59864b72d5fd614897e
SmartWalle <smartwalle@gmail.com>
2017-01-18 18:00:22 +0800

    添加 RSA 签名和验证签名的方法

--- http/request.go
+++ http/request.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"fmt"
 )
 
 func NewRequest(method, url string, params url.Values) (*http.Request, error) {
@@ -54,6 +55,7 @@ func DoJSONRequest(c *http.Client, req *http.Request, result interface{}) (*http
 	if err != nil {
 		return rep, err
 	}
+	fmt.Println(string(repBody))
 	err = json.Unmarshal(repBody, &result)
 	return rep, err
 }
